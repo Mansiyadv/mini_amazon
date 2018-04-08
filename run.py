b89f90351d2237721cf5cdca487b367d9b2b9ec4
Mansiyadv <mansiyadv17@gmail.com>
2018-04-08 15:54:46 +0530

    has search option

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, send_from_directory, request, Response
 
 app = Flask('mini-amazon', static_url_path='')
+product_list = []
 
 
 @app.route('/health', methods=['GET'])
@@ -13,17 +14,25 @@ def index():
     return send_from_directory('static', 'index.html')
 
 
-@app.route('/api/products', methods=['POST'])
+@app.route('/api/products', methods=['POST', 'GET'])
 def products():
-    product = dict()
-    product['Name'] = request.form['Name']
-    product['Description'] = request.form['Description']
-    product['Price'] = request.form['Price']
-    product['Email ID'] = request.form['Email ID']
+    if request.method == 'GET':
+        print(product_list)
+        print(request.args['Name'])
+        for product in product_list:
+            if product['Name'] == request.args['Name']:
+                return Response(str(product), 200)
+        return Response(str({}), 200)
+    elif request.method == 'POST':
+        product = dict()
+        product['Name'] = request.form['Name']
+        product['Description'] = request.form['Description']
+        product['Price'] = request.form['Price']
+        print(product)
 
-    print(product)
+        product_list.append(product)
 
-    return Response('OK', 200)
+        return Response('OK', 200)
 
 
 if __name__ == '__main__':
