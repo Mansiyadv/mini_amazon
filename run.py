bfec04c992ae5cafa092c163b60229d2b925092f
Mansiyadv <mansiyadv17@gmail.com>
2018-04-14 17:15:58 +0530

    using mongodb

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from flask import Flask, send_from_directory, request, Response
+import pymongo
+from pymongo import MongoClient
+
+client= MongoClient('localhost',27017)
+db= client.mini_amazon
 
 app = Flask('mini-amazon', static_url_path='')
-product_list = []
+#product_list = []
 
 
 @app.route('/health', methods=['GET'])
@@ -15,25 +20,31 @@ def index():
 
 
 @app.route('/api/products', methods=['POST', 'GET'])
+
 def products():
     if request.method == 'GET':
-        print(product_list)
-        print(request.args['Name'])
-        for product in product_list:
-            if product['Name'] == request.args['Name']:
-                return Response(str(product), 200)
-        return Response(str({}), 200)
+        #print(product_list)
+        #print(request.args['name'])
+        match_prod=db.products.find({'Name':  request.args['Name']})
+        matches=[]
+
+        for prod in match_prod:
+            matches.append(prod)
+               
+        return Response(str(matches), mimetype="application/json", status=200)
     elif request.method == 'POST':
         product = dict()
         product['Name'] = request.form['Name']
         product['Description'] = request.form['Description']
         product['Price'] = request.form['Price']
-        print(product)
+        #print(product)
 
-        product_list.append(product)
+        db.products.insert_one(product)
 
-        return Response('OK', 200)
+        return Response(str({'status':'success'}), mimetype="application/json", status=200)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+json_obj = {'Name':''}
